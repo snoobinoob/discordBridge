88016d5ee214dde3144e1d00c12124178beebf7c
Snoobinoob <cheeseycheese99@gmail.com>
2026-02-11 18:49:48 -0500

    Initial commit

---
+++ src/main/java/discordbridge/DiscordBridgeEntry.java
@@ -0,0 +1,11 @@
+package discordbridge;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class DiscordBridgeEntry {
+
+    public void init() {
+        System.out.println("Hello world from discord bridge!");
+    }
+}
